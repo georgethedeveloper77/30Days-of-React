d7955854f8afa49b447217fa980e81be55ce694d
georgethedeveloper77 <georgekarani77@gmail.com>
2020-07-27 21:24:42 +0300

    getCountriesDAta req=>

--- Projects/covid-19-tracker2/src/App.js
+++ Projects/covid-19-tracker2/src/App.js
@@ -1,11 +1,63 @@
-import React from 'react';
-
-import './App.css';
+import React, { useState, useEffect } from "react";
+import { MenuItem, FormControl, Select } from "@material-ui/core";
+import "./App.css";
 
 function App() {
+  //useState = set variable
+  //useEffect = run a piece of code base on given condition
+  // https://disease.sh/v3/covid-19/countries
+  const [countries, setCountries] = useState([]);
+  const [country, setCountry] = useState("worldwide");
+
+  //runs once when componet loads
+  useEffect(() => {
+    // async => send a request
+    const getCountriesData = async () => {
+      await fetch(" https://disease.sh/v3/covid-19/countries")
+        .then((response) => response.json())
+        .then((data) => {
+          const countries = data.map((country) => ({
+            name: country.country,
+            value: country.countryInfo.iso2,
+          }));
+          setCountries(countries);
+        });
+    };
+    getCountriesData();
+  }, []);
+
+  const onCountryChange = (event) => {
+    //async
+    const countryCode = event.target.value;
+    setCountry(countryCode);
+  };
+
   return (
     <div className="app">
-      <h1>Covid 19</h1>
+      <div className="app_header">
+        <FormControl className="app_dropdown">
+          <Select variant="outlined" onChange={onCountryChange} value={country}>
+            <MenuItem value="worldwide">Global</MenuItem>
+            {countries.map((country) => (
+              <MenuItem value={country.value}>{country.name}</MenuItem>
+            ))}
+          </Select>
+        </FormControl>
+      </div>
+
+      <div className="app_stats">
+
+      </div>
+
+
+      {/* InfoBoxs*/}
+      {/* InfoBoxs*/}
+      {/* InfoBoxs*/}
+
+      {/* Table*/}
+      {/* Graps*/}
+
+      {/* InfoBoxs*/}
     </div>
   );
 }
