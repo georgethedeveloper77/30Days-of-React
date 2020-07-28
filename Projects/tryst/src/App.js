45b830ded643bfede97e14464d752d0816564725
georgethedeveloper77 <georgekarani77@gmail.com>
2020-07-28 13:08:55 +0300

    setup files header

---
+++ Projects/tryst/src/App.js
@@ -0,0 +1,19 @@
+import React from "react";
+import "./App.css";
+import Header from "./Header";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>hello</h1>
+      {/*header*/}
+      <Header />
+      {/*tinder Cards*/}
+      {/* Buttons below tinder*/}
+
+      {/*Chat Screen*/}
+    </div>
+  );
+}
+
+export default App;
---
+++ Projects/tryst/src/Header.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import PersonIcon from '@materaial-ui/icons/Person'
+
+export default function Header() {
+  return (
+    <div>
+      <h1>Header</h1>
+    </div>
+  )
+}
