bd2066367234fb66bbeef0ee9d72ebc5b3e8e648
georgethedeveloper77 <georgekarani77@gmail.com>
2020-08-01 00:56:17 +0300

    build n deploy

--- Projects/arcade/src/comps/Title.js
+++ Projects/arcade/src/comps/Title.js
@@ -1,13 +1,13 @@
-import React from 'react';
+import React from "react";
 
 const Title = () => {
   return (
     <div className="title">
       <h1>ArCade</h1>
-      <h2>MY Pictures</h2>
+      <h2>My Pics</h2>
       <p>They are beautiful</p>
     </div>
-  )
-}
+  );
+};
 
 export default Title;
