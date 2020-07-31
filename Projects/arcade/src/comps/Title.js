af57b0b88e8ec5283c410b6a8e97d5b67a52e455
georgethedeveloper77 <georgekarani77@gmail.com>
2020-07-31 22:31:08 +0300

    Title...

---
+++ Projects/arcade/src/comps/Title.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Title = () => {
+  return (
+    <div className="title">
+      <h1>ArCade</h1>
+      <h2>MY Pictures</h2>
+      <p>They are beautiful</p>
+    </div>
+  )
+}
+
+export default Title;
