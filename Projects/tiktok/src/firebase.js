a3eb4160e26d57dd7eb99c197c8081f53b0da1dd
georgethedeveloper77 <georgekarani77@gmail.com>
2020-08-03 19:17:29 +0300

    setup=> files

--- Projects/tiktok/src/firebase.js
+++ Projects/tiktok/src/firebase.js
@@ -1,3 +1,5 @@
+import firebase from "firebase";
+
 const firebaseConfig = {
   apiKey: "AIzaSyAb1WCpiRMd_I-Mx5kgayBkzcavrMeBeuI",
   authDomain: "splurge-9af52.firebaseapp.com",
@@ -8,3 +10,9 @@ const firebaseConfig = {
   appId: "1:944411296779:web:d3ef498cde94444f93330e",
   measurementId: "G-DZE1JJG8BZ"
 };
+
+const firebaseApp = firebase.initializeApp(firebaseConfig);
+
+const db = firebaseApp.firestore();
+
+export default db;
